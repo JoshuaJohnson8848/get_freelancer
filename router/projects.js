34fdc09180104074f9c2abb3410fabaa4068a8bd
joshua-pathkraft <126854752+joshua-pathkraft@users.noreply.github.com>
2024-02-16 18:47:14 +0530

    Completed Projects Section

--- router/projects.js
+++ router/projects.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const isAuth = require('../middleware/isAuth')
 const projectController = require('../controller/projects');
 
+router.get('', isAuth, projectController.getProjects);
+
+router.get('/:id', isAuth, projectController.getProjectById);
+
+router.get('/byUser', isAuth, projectController.getProjectByUserId);
+
 router.post('', isAuth, projectController.createProject);
+
+router.put('/:id', isAuth, projectController.updateProject);
+
+router.delete('/:id', isAuth, projectController.deleteProject);
 
 module.exports = router;
