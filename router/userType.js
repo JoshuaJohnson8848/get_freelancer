d5765e66976d75b5824ec91532ce7a530c46dc3a
joshua-pathkraft <joshuatjohnson255@gmail.com>
2024-02-17 16:16:28 +0530

    Added Extra Features in UserTypes

--- router/userType.js
+++ router/userType.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userTypeController = require('../controller/userType');
+const isAuth = require('../middleware/isAuth');
 
-router.post('',userTypeController.createUserType);
+router.get('', isAuth, userTypeController.getAll);
+
+router.post('', isAuth, userTypeController.createUserType);
+
+router.put('/:id', isAuth, userTypeController.update);
+
+router.delete('/:id', isAuth, userTypeController.delete);
 
 module.exports = router;
